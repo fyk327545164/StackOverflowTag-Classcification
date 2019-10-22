def788dbaab4de5bb66ed2204ccc0d317c6a71d5
Kunn <44954787+fyk327545164@users.noreply.github.com>
2019-10-22 17:58:06 -0400

    commit TextRNN, TextRCNN models

--- Models.py
+++ Models.py
@@ -9,9 +9,9 @@ class FastText(tf.keras.Model):
 
         self.embedding = tf.keras.layers.Embedding(VOCAB_SIZE + 1, self.config.EMBEDDING_DIM, mask_zero=True)
 
-        self.dense1 = tf.keras.layers.Dense(self.config.HIDDEN_DIM, input_shape=(self.config.EMBEDDING_DIM,))
+        self.dense1 = tf.keras.layers.Dense(self.config.HIDDEN_DIM, input_shape=(None, self.config.EMBEDDING_DIM))
 
-        self.dense2 = tf.keras.layers.Dense(self.config.OUTPUT_DIM, input_shape=(self.config.HIDDEN_DIM,))
+        self.dense2 = tf.keras.layers.Dense(self.config.OUTPUT_DIM, input_shape=(None, self.config.HIDDEN_DIM))
 
         self.softmax = tf.keras.activations.softmax()
 
@@ -50,7 +50,7 @@ class TextCNN(tf.keras.Model):
 
         self.dropout = tf.keras.layers.Dropout(0.5)
 
-        self.dense = tf.keras.layers.Dense(self.config.OUTPUT_DIM, input_shape=(self.config.NUM_CHANNELS * 3,))
+        self.dense = tf.keras.layers.Dense(self.config.OUTPUT_DIM, input_shape=(None, self.config.NUM_CHANNELS * 3))
 
         self.softmax = tf.keras.activations.softmax()
 
@@ -77,6 +77,31 @@ class TextRNN(tf.keras.Model):
 
         self.embedding = tf.keras.layers.Embedding(VOCAB_SIZE + 1, self.config.EMBEDDING_DIM, mask_zero=True)
 
+        self.forward_LSTM = tf.keras.layers.LSTM(self.config.RNN_DIM)
+        self.backward_LSTM = tf.keras.layers.LSTM(self.config.RNN_DIM, go_backwards=True)
+
+        self.dropout = tf.keras.layers.Dropout(0.5)
+
+        self.dense = tf.keras.layers.Dense(self.config.OUTPUT_DIM, input_shape=(None, self.config.RNN_DIM*2))
+
+        self.softmax = tf.keras.activations.softmax()
+
+    def call(self, inputs, training=None, mask=None):
+
+        embed = tf.transpose(self.embedding(inputs), perm=[1, 0, 2])
+
+        forward_out = self.forward_LSTM(embed)
+        backward_out = self.backward_LSTM(embed)
+
+        out = tf.concat([forward_out, backward_out], axis=-1)
+
+        if training:
+            out = self.dropout(out)
+
+        out = self.dense1(out)
+
+        return self.softmax(out)
+
 
 class TextRCNN(tf.keras.Model):
     def __init__(self, config, VOCAB_SIZE):
@@ -84,6 +109,38 @@ class TextRCNN(tf.keras.Model):
         self.config = config
 
         self.embedding = tf.keras.layers.Embedding(VOCAB_SIZE + 1, self.config.EMBEDDING_DIM, mask_zero=True)
+
+        self.forward_LSTM = tf.keras.layers.LSTM(self.config.RNN_DIM, return_sequences=True)
+        self.backward_LSTM = tf.keras.layers.LSTM(self.config.RNN_DIM, return_sequences=True, go_backwards=True)
+
+        self.dropout = tf.keras.layers.Dropout(0.5)
+
+        self.dense1 = tf.keras.layers.Dense(self.config.HIDDEN_DIM, input_shape=(None, self.config.RNN_DIM * 2 + self.config.EMBEDDING_DIM),
+                                            activation='tanh')
+
+        self.dense2 = tf.keras.layers.Dense(self.config.OUTPUT_DIM, input_shape=(None, self.config.HIDDEN_DIM))
+
+        self.softmax = tf.keras.activations.softmax()
+
+    def call(self, inputs, training=None, mask=None):
+        embed = tf.transpose(self.embedding(inputs), perm=[1, 0, 2])
+
+        forward_out = self.forward_LSTM(embed)
+        backward_out = self.backward_LSTM(embed)
+
+        out = tf.transpose(tf.concat([forward_out, tf.transpose(inputs, perm=[1, 0, 2]), backward_out], axis=-1),
+                           perm=[1, 0, 2])
+
+        out = tf.transpose(self.dense1(out), perm=[0, 2, 1])
+
+        out = tf.keras.layers.MaxPool1D(tf.shape(out)[-1])(out)
+
+        if training:
+            out = self.dropout(out)
+
+        out = self.dense2(tf.squeeze(out, axis=-1))
+
+        return self.softmax(out)
 
 
 class TextRNNAttention(tf.keras.Model):
